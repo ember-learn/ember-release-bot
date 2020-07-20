f8956779469dcafa267867367fd66c0c3f942246
Chris Manson <chris@manson.ie>
2020-07-20 10:12:32 +0100

    adding some testing and fixing messages

--- cron/daily-message.js
+++ cron/daily-message.js
@@ -27,18 +27,28 @@ module.exports = {
 
       // more than 5 days means that it's not release week
       if (dayDiff === -11) {
-        channel.send(`Release week starts next week on ${dateString} are we all prepared? :lts:`);
+        channel.send(`Release week starts next week on ${moment(dateString).format('YYYY-MM-DD')} are we all prepared? :lts:`);
       } else if (dayDiff === -4) {
         channel.send(`:tada: It's release week!! :tada: To see who is done and who isn't you can say '!release done' and I'll tell you who still needs to do something!
 
 If you know a team is done you can say '!release done <team>' where team can be blog, framework, data, or cli`);
       } else if (dayDiff === -2) {
-        // eslint-disable-next-line quotes
-        channel.send(`We're half way through release week! Remember: you can say '!release done' to see who still needs to release!`);
+        if (waitingFor.length) {
+          // eslint-disable-next-line quotes
+          channel.send(`We're half way through release week! Remember: you can say '!release done' to see who still needs to release!`);
+        } else {
+          channel.send('Wow! :tada: we have released everything within 3 days!? This must be some sort of record :scream: Congratulations to everyone who helped out! :tada:');
+        }
       } else if (dayDiff === 0) {
-        channel.send(`Today is the last day of release week! We're still waiting on ${waitingFor.join(', ')}. Can we release today?`);
+        if (waitingFor.length) {
+          channel.send(`Today is the last day of release week! We're still waiting on ${waitingFor.join(', ')}. Can we release today?`);
+        } else {
+          channel.send(':tada: We have released everything on time this week :tada: This is worth a bit of a celebration! :tada: Great work team :muscle:');
+        }
       } else if (dayDiff > 0) {
-        channel.send(`We are currently :rotating_light: ${dayDiff} Days Late :rotating_light: with the release!! We are still waiting on ${waitingFor.join(', ')}`);
+        if (waitingFor.length) {
+          channel.send(`We are currently :rotating_light: ${dayDiff + 1} Days Late :rotating_light: with the release!! We are still waiting on ${waitingFor.join(', ')}`);
+        }
       }
     };
   },
