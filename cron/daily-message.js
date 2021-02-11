b264f447cba80675d21933656571498a707e27a1
Chris Manson <chris@manson.ie>
2021-02-11 16:30:25 +0000

    fix daily-message linting, 24 hour window, clarify variable names

--- cron/daily-message.js
+++ cron/daily-message.js
@@ -1,56 +1,56 @@
 const moment = require('moment');
 const _ = require('lodash');
 
-function createMessage({dateString, waitingFor}) {
+function isReleaseInXDays(xDays, hoursUntilRelease) {
+  const hourLowerBound = (24 * xDays) - 12;
+  const hourUpperBound = (24 * xDays) + 12;
+
+  return (hourLowerBound <= hoursUntilRelease) && (hoursUntilRelease <= hourUpperBound);
+}
+
+function createMessage({ dateString, waitingFor }) {
   if (!dateString) {
     return;
   }
 
-  const releaseDate = moment(dateString).add(4, 'days');
-  const hoursUntilRelease = releaseDate.diff(moment(), 'hours');
+  const lastDayOfReleaseWeek = moment.utc(dateString).add(4, 'days');
+  const today = moment().utc();
+  const daysUntilRelease = lastDayOfReleaseWeek.diff(today, 'days');
+  const hoursUntilRelease = lastDayOfReleaseWeek.diff(today, 'hours');
 
   if (isReleaseInXDays(11, hoursUntilRelease)) {
     return `Release week starts next week on ${moment(dateString).format('YYYY-MM-DD')} are we all prepared? :lts:`;
   }
 
   if (isReleaseInXDays(4, hoursUntilRelease)) {
-    return [
-      `:tada: It's release week!! :tada: To see who is done and who isn't you can say '!release done' and I'll tell you who still needs to do something!`,
-      '',
-      `If you know a team is done you can say '!release done <team>' where team can be blog, framework, data, or cli.`,
-    ].join('\n');
+    return `:tada: It's release week!! :tada: To see who is done and who isn't you can say '!release done' and I'll tell you who still needs to do something!
+
+If you know a team is done you can say '!release done <team>' where team can be blog, framework, data, or cli.`;
   }
 
   if (isReleaseInXDays(2, hoursUntilRelease)) {
     if (waitingFor.length) {
       // eslint-disable-next-line quotes
       return `We're half way through release week! Remember: you can say '!release done' to see who still needs to release!`;
-    } else {
-      return 'Wow! :tada: we have released everything within 3 days!? This must be some sort of record :scream: Congratulations to everyone who helped out! :tada:';
     }
+
+    return 'Wow! :tada: we have released everything within 3 days!? This must be some sort of record :scream: Congratulations to everyone who helped out! :tada:';
   }
 
   if (isReleaseInXDays(0, hoursUntilRelease)) {
     if (waitingFor.length) {
       return `Today is the last day of release week! We're still waiting on ${waitingFor.join(', ')}. Can we release today?`;
-    } else {
-      return ':tada: We have released everything on time this week :tada: This is worth a bit of a celebration! :tada: Great work team :muscle:';
     }
+
+    return ':tada: We have released everything on time this week :tada: This is worth a bit of a celebration! :tada: Great work team :muscle:';
   }
 
-  if (hoursUntilRelease < 0) {
+  if (daysUntilRelease < 0) {
     if (waitingFor.length) {
-      const daysAfterRelease = Math.round(-1 * hoursUntilRelease / 24);
-      return `We are currently :rotating_light: ${daysAfterRelease} Days Late :rotating_light: with the release!! We are still waiting on ${waitingFor.join(', ')}`;
+      // const daysAfterRelease = Math.round(-1 * hoursUntilRelease / 24);
+      return `We are currently :rotating_light: ${Math.abs(daysUntilRelease)} Days Late :rotating_light: with the release!! We are still waiting on ${waitingFor.join(', ')}`;
     }
   }
-}
-
-function isReleaseInXDays(xDays, hoursUntilRelease) {
-  const hourLowerBound = 24 * xDays - 2;
-  const hourUpperBound = 24 * xDays + 2;
-
-  return (hourLowerBound <= hoursUntilRelease) && (hoursUntilRelease <= hourUpperBound);
 }
 
 module.exports = {
