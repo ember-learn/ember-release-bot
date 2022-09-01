e2e25e78dba0a8bbdc039ed2686165511a1c7118
Chris Manson <chris@manson.ie>
2022-09-01 10:19:13 +0100

    convert commands into slash commands

--- commands/info.js
+++ commands/info.js
@@ -1,7 +1,12 @@
 const moment = require('moment');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-  async execute(message, args, keyv) {
+  data: new SlashCommandBuilder()
+    .setName('release-info')
+    .setDescription('returns info about the current release'),
+
+  async execute(interaction, keyv) {
     const version = await keyv.get('version');
     const dateString = await keyv.get('date');
 
@@ -11,11 +16,11 @@ module.exports = {
 
     // more than 5 days means that it's not release week
     if (dayDiff < -5) {
-      message.channel.send(`The next release version is ${version} and is planned for the week starting roughly ${date.toNow(true)} from now`);
+      interaction.reply(`The next release version is ${version} and is planned for the week starting roughly ${date.subtract(4, 'days').toNow(true)} from now`);
     } else if (dayDiff < 0) {
-      message.channel.send(`The next release is ${version} and is currently being worked on this week :tada:`);
+      interaction.reply(`The next release is ${version} and is currently being worked on this week :tada:`);
     } else {
-      message.channel.send(`The next release version is ${version} and is currently :rotating_light: ${dayDiff} Days Late :rotating_light:`);
+      interaction.reply(`The next release version is ${version} and is currently :rotating_light: ${dayDiff} Days Late :rotating_light:`);
     }
   },
 };
